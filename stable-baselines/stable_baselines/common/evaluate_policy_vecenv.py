7087884f459de2d0bed8f117e4ac0b235ab8125a
HDClark94 <harrydclark91@gmail.com>
2019-06-04 10:23:26 +0100

    flipped using from using subpro to dummyvec env in acer_simple and acer_bi_action_rnnsubproc

--- stable-baselines/stable_baselines/common/evaluate_policy_vecenv.py
+++ stable-baselines/stable_baselines/common/evaluate_policy_vecenv.py
@@ -57,19 +57,24 @@ def evaluate_policy_vecenv(model, env, seed=None):
 
     elif model.policy == MlpLstmPolicy:
 
-        obs, done = env.reset(), False
+        #obs, done = env.reset(), False
+        obs = env.reset()
+        done = [False]
+
         episode_rew = 0
         episode_log = []
         layer_log = []
         action_log = []
         value_log = []
 
-        trial_type = env.get_attr('trialtype')[0]
-        #trial_type = env.envs[0].env.trialtype
+        #trial_type = env.get_attr('trialtype')[0]
+        trial_type = env.envs[0].env.trialtype
+
+        _state = None
 
         while not done:
             # env.render()
-            action, obs, layers_list, value = model.predict(obs, deterministic=True)
+            action, _state, layers_list, value = model.predict(obs, state=_state, mask=done, deterministic=True)
             obs, rew, done, state = env.step(action)
 
             # store log for greedy
@@ -81,7 +86,7 @@ def evaluate_policy_vecenv(model, env, seed=None):
 
             done = done[0] # were only interested in the first vectorized environment
 
-        print(episode_rew, "=episode reward")
+        #print(episode_rew, "=episode reward")
 
         env.close()
         return episode_log, episode_rew, layer_log, action_log, value_log, trial_type
