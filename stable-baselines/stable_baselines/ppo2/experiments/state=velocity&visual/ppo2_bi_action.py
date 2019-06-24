bcb536bce7135f2e7f0758e2ccaab903b2c0701b
hdclark94 <harrydclark91@gmail.com>
2019-06-24 08:56:07 +0100

    state velo and indicator with recurrent

--- stable-baselines/stable_baselines/ppo2/experiments/state=velocity&visual/ppo2_bi_action.py
+++ stable-baselines/stable_baselines/ppo2/experiments/state=velocity&visual/ppo2_bi_action.py
@@ -4,7 +4,7 @@ import gym
 from stable_baselines.common.policies import MlpPolicy, MlpLstmPolicy, MlpLnLstmPolicy
 from stable_baselines.common.vec_env import SubprocVecEnv
 from stable_baselines.common.vec_env import DummyVecEnv
-from stable_baselines import PPO1
+from stable_baselines import PPO2
 import numpy as np
 from stable_baselines.common.plotting import *
 import os
@@ -12,16 +12,17 @@ import os
 dir = os.path.dirname(__file__)
 plot_path = os.path.join(dir, 'figures', 'binary_action', '')
 
-action_errors = [0, 0.001, 0.01, 0.1]
+action_errors = [0, 0.01, 0.1, 1]
+action_errors = [0]
 training_steps = 400000
 
-print("running PPO1")
+print("running PPO2")
 
 # with error
 # multiprocess environment
-
-env_string = 'state_velovisual_MountainCar-v0'
-id = 300
+policy = MlpLstmPolicy
+env_string ='state_velovisual_MountainCar-v0'
+id = 20
 
 for std in action_errors:
 
@@ -30,19 +31,25 @@ for std in action_errors:
     env.set_obs_error(std)
     env = DummyVecEnv([lambda: env])
 
-    for i in range(3):
+    for i in range(10):
         std_str = "".join(str(std).split("."))
 
         id_string = str(id).rjust(4, "0")
         title = "id=" + id_string + "_std=" + std_str + "_i=" + str(i)
         print("Processing std = ", std)
 
-        model = PPO1(MlpPolicy, env, verbose=0, action_error_std=std)
+        model = PPO2(policy, env, verbose=0, action_error_std=std, nminibatches=1)
         model.learn(total_timesteps=training_steps, eval_env_string=env_string)
 
         # for plotting
-        plot_summary_with_fn(model.ep_logs, model.value_log, model.trialtype_log, plot_path, title)
-        plot_network_activation(model.layer_log, model.ep_logs, model.trialtype_log, plot_path, title + "_last_trial_layer_")
+        if policy == MlpPolicy:
+            plot_summary_with_fn(model.ep_logs, model.value_log, model.trialtype_log, plot_path, title)
+            plot_network_activation(model.layer_log, model.ep_logs, model.trialtype_log, plot_path,
+                                title + "_last_trial_layer_")
+        elif policy == MlpLstmPolicy or policy == MlpLnLstmPolicy:
+            plot_summary_with_fn(model.ep_logs, model.value_log, model.trialtype_log, plot_path, title)
+            plot_network_activation_rnn(model.layer_log, model.ep_logs, model.trialtype_log, plot_path,
+                                    title + "_last_trial_layer_")
 
         del model # remove to demonstrate saving and loading
         id += 1
--- stable-baselines/stable_baselines/ppo2/experiments/state=velocity&visual/ppo2_cont_action.py
+++ stable-baselines/stable_baselines/ppo2/experiments/state=velocity&visual/ppo2_cont_action.py
@@ -4,7 +4,7 @@ import gym
 from stable_baselines.common.policies import MlpPolicy, MlpLstmPolicy, MlpLnLstmPolicy
 from stable_baselines.common.vec_env import SubprocVecEnv
 from stable_baselines.common.vec_env import DummyVecEnv
-from stable_baselines import PPO1
+from stable_baselines import PPO2
 import numpy as np
 from stable_baselines.common.plotting import *
 import os
@@ -13,12 +13,13 @@ from stable_baselines.common.misc_util import set_global_seeds
 dir = os.path.dirname(__file__)
 plot_path = os.path.join(dir, 'figures', 'trinary_action', '')
 
-action_errors = [0, 0.001, 0.01, 0.1]
-training_steps = 4000
+action_errors = [0, 0.01, 0.1, 1]
+action_errors = [0]
+training_steps = 400000
 seed = 3
-print("running PPO1")
-id = 300
-
+print("running PPO2")
+id = 20
+policy = MlpLstmPolicy
 # with error
 env_string = 'state_velovisual_MountainCarContinuous-v0'
 
@@ -32,20 +33,25 @@ for std in action_errors:
 
     env = DummyVecEnv([lambda: env])
 
-    for i in range(3):
+    for i in range(10):
         std_str = "".join(str(std).split("."))
 
         id_string = str(id).rjust(4, "0")
         title = "id=" + id_string + "_std=" + std_str + "_i=" + str(i)
         print("Processing std = ", std)
 
-        model = PPO1(MlpPolicy, env, verbose=0, action_error_std=std)
+        model = PPO2(policy, env, verbose=0, action_error_std=std, nminibatches=1)
         model.learn(total_timesteps=training_steps, eval_env_string=env_string, seed=seed)
 
         # for plotting
-        plot_summary_with_fn(model.ep_logs, model.value_log, model.trialtype_log, plot_path, title)
-        plot_network_activation(model.layer_log, model.ep_logs, model.trialtype_log, plot_path,
-                                title + "_last_trial_layer_")
+        if policy == MlpPolicy:
+            plot_summary_with_fn(model.ep_logs, model.value_log, model.trialtype_log, plot_path, title)
+            plot_network_activation(model.layer_log, model.ep_logs, model.trialtype_log, plot_path,
+                                    title + "_last_trial_layer_")
+        elif policy == MlpLstmPolicy or policy == MlpLnLstmPolicy:
+            plot_summary_with_fn(model.ep_logs, model.value_log, model.trialtype_log, plot_path, title)
+            plot_network_activation_rnn(model.layer_log, model.ep_logs, model.trialtype_log, plot_path,
+                                        title + "_last_trial_layer_")
 
         del model # remove to demonstrate saving and loading
         id += 1
